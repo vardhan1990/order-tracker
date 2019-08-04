9b26424fa6b4139abda563bf8621cb46cc5be586
Vardhan Dugar <vardhan1990@gmail.com>
2019-08-03 22:49:39 -0700

    renames

--- src/web-api/api.js
+++ src/web-api/api.js
@@ -1,9 +1,9 @@
 import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:8000');
 
-function emitReadyEventOnSocket(newCardsCallback, timerCallback) {
-    socket.on('newCards', newCards => newCardsCallback(JSON.parse(newCards)));
-    socket.on('timer', timer => timerCallback(timer));
+function emitReadyEventOnSocket(newUpdatesCallback, clockTickCallback) {
+    socket.on('newUpdates', newUpdates => newUpdatesCallback(JSON.parse(newUpdates)));
+    socket.on('clockTick', clockTick => clockTickCallback(clockTick));
     socket.emit('ready');
   } 
 
