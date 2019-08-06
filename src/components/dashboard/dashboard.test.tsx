25387a86810ca82535a9c0be3d69d103d760aa03
Vardhan Dugar <vardhan1990@gmail.com>
2019-08-06 01:59:57 -0700

    more tests

--- src/components/dashboard/dashboard.test.tsx
+++ src/components/dashboard/dashboard.test.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import { Dashboard } from './dashboard';
 import { Provider, themes } from '@stardust-ui/react';
 import * as constants from '../constants';
+import { Accordion, Button, Flex, Header, Input, Text } from '@stardust-ui/react';
 
 describe("Dashboard", () => {
-  const getCurrentTimeFn = () => {
-     return 1;
-  };
+  let wrapper: ReactWrapper;
 
-  const sendUpdateFn = (id: string, name: string, destination: string, event_name: constants.EventNameType) => {};
+  beforeEach(() => {
+    const getCurrentTimeFn = () => {
+      return 1;
+   };
+ 
+   const sendUpdateFn = (id: string, name: string, destination: string, event_name: constants.EventNameType) => {};
 
-  it('renders without crashing', () => {
     const div = document.createElement('div');
 
-    mount(
+    wrapper = mount(
         <Provider theme={themes.teams}>
             <Dashboard getCurrentTime={getCurrentTimeFn} sendUpdateFn={sendUpdateFn} />
         </Provider>
     , div);
+  });
+
+  it('renders without crashing', () => {});
+
+  it('rendered element is defined', () => {
+    expect(wrapper).toBeDefined();
   });
 });
--- src/components/update/update.test.tsx
+++ src/components/update/update.test.tsx
@@ -6,23 +6,47 @@ import * as constants from '../constants';
 import { IUpdate } from '../../update.interface';
 
 describe("Update", () => {
-  const latestUpdate: IUpdate[] = [{
-    id: "id",
-    name: "string",
-    event_name: "CREATED",
-    sent_at_second: 1,
-    destination: "string"
-  }];
+  let wrapper: ReactWrapper;
 
-  const sendUpdateFn = (id: string, name: string, destination: string, event_name: constants.EventNameType) => {};
+  beforeEach(() => {
+    const latestUpdate: IUpdate[] = [{
+      id: "id",
+      name: "string",
+      event_name: "CREATED",
+      sent_at_second: 1,
+      destination: "string"
+    }];
 
-  it('renders without crashing', () => {
+    const sendUpdateFn = (id: string, name: string, destination: string, event_name: constants.EventNameType) => {};
+
     const div = document.createElement('div');
 
-    mount(
-        <Provider theme={themes.teams}>
-            <Update latestUpdate={latestUpdate} id={"id"} sendUpdateFn={sendUpdateFn} />
-        </Provider>
+    wrapper = mount(
+      <Provider theme={themes.teams}>
+          <Update latestUpdate={latestUpdate} id={"id"} sendUpdateFn={sendUpdateFn} />
+      </Provider>
     , div);
+  });
+  
+  it('renders without crashing', () => {});
+
+  it('rendered element is defined', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it(`setting props doesn't crash`, () => {
+    const latestUpdate: IUpdate[] = [{
+      id: "newId",
+      name: "string",
+      event_name: "CREATED",
+      sent_at_second: 1,
+      destination: "string"
+    }];
+
+
+    wrapper.setProps({
+      latestUpdate: {latestUpdate},
+      id: 'newId'
+    });
   });
 });
