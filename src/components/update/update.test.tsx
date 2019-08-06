b83344bbeb32a3afa6fca73b2cd738944e5222e0
Vardhan Dugar <vardhan1990@gmail.com>
2019-08-06 00:19:44 -0700

    update

--- src/components/update/update.test.tsx
+++ src/components/update/update.test.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { Dashboard } from './dashboard';
+import { Update } from './update';
 import { Provider, themes } from '@stardust-ui/react';
 import * as constants from '../constants';
+import { IUpdate } from '../../update.interface';
 
-describe("Dashboard", () => {
-  const getCurrentTimeFn = () => {
-     return 1;
-  };
+describe("Update", () => {
+  const latestUpdate: IUpdate[] = [{
+    id: "id",
+    name: "string",
+    event_name: "CREATED",
+    sent_at_second: 1,
+    destination: "string"
+  }];
 
   const sendUpdateFn = (id: string, name: string, destination: string, event_name: constants.EventNameType) => {};
 
@@ -16,7 +21,7 @@ describe("Dashboard", () => {
     
     mount(
         <Provider theme={themes.teams}>
-            <Dashboard getCurrentTime={getCurrentTimeFn} sendUpdateFn={sendUpdateFn} />
+            <Update latestUpdate={latestUpdate} id={"id"} sendUpdateFn={sendUpdateFn} />
         </Provider>
     , div);
   });
