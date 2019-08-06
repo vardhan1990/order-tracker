0da79501747031fb938af4ec5430219c77c9b273
Vardhan Dugar <vardhan1990@gmail.com>
2019-08-05 23:47:25 -0700

    card test

---
+++ src/components/card/card.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Card } from './card';
+import { IUpdate } from '../../update.interface';
+import { Provider, themes } from '@stardust-ui/react';
+
+describe("Card", () => {
+const update: IUpdate = {
+    id: "id",
+    name: "string",
+    event_name: "CREATED",
+    sent_at_second: 1,
+    destination: "string"
+}
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    
+    mount(
+        <Provider theme={themes.teams}>
+            <Card {...update} />
+        </Provider>
+    , div);
+  });
+});
